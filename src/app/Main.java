7285e9c5730608fe840bb9c149c8f7f565cdac01
Vladyslav Matsenko <ex65039@gmail.com>
2023-01-12 16:50:21 +0200

    Add divide and subtraction methods

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,15 +2,26 @@ package app;
 
 public class Main {
 
+    static int a;
+    static int b;
+    static int c;
+    static int d;
+    static double k;
+    static int l;
+
     public static void main(String[] args) {
         System.out.println("My app for Users!");
         System.out.println("Version 1.0");
-        int a = 8;
-        int b = 2;
-        int c = add(a, b);
+        a = 8;
+        b = 2;
+        c = add(a, b);
         System.out.println(c);
-        int d = multiply(a, b);
+        d = multiply(a, b);
         System.out.println(d);
+        k = divide(a, b);
+        System.out.println(k);
+        l = subtraction(a, b);
+        System.out.println(l);
     }
 
     private static int add(int a, int b) {
@@ -20,4 +31,13 @@ public class Main {
     private static int multiply(int a, int b) {
         return a * b;
     }
+
+    private static double divide(double a, double b) {
+        return a / b;
+    }
+
+    private static int subtraction(int a, int b) {
+        return a - b;
+    }
 }
+
